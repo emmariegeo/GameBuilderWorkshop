9fcdb6bbce073a610da11f6258710bc11105173a
emmariegeo <57061691+emmariegeo@users.noreply.github.com>
2023-10-23 11:46:11 -0700

    Navigation bar - Initial

--- web-app/src/app/page.tsx
+++ web-app/src/app/page.tsx
@@ -1,9 +1,12 @@
+"use client"
 import Canvas from '@/components/Canvas'
 import styles from './page.module.css'
+import Navigation from '@/components/Navigation'
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Navigation>
       <div className={styles.description}>
         <div>
           Game Builder Workshop
@@ -13,6 +16,7 @@ export default function Home() {
       <div className={styles.center}>
         <Canvas/>
       </div>
+      </Navigation>
     </main>
   )
 }
---
+++ web-app/src/components/Navigation.tsx
@@ -0,0 +1,85 @@
+import { Box, CssBaseline, AppBar, Toolbar, IconButton, Typography, Button, Divider, ListItemButton, ListItem, List, ListItemText, Drawer } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from 'react';
+
+export default function Navigation({ children }) {
+    const drawerWidth = 240;
+    const navItems = ['Help'];
+
+    const [mobileOpen, setMobileOpen] = useState(false);
+
+    const handleDrawerToggle = () => {
+        setMobileOpen((prevState) => !prevState);
+    };
+
+    const drawer = (
+        <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+            <Typography variant="h6" sx={{ my: 2 }}>
+                Game Builder Workshop
+            </Typography>
+            <Divider />
+            <List>
+                {navItems.map((item) => (
+                    <ListItem key={item} disablePadding>
+                        <ListItemButton sx={{ textAlign: 'center' }}>
+                            <ListItemText primary={item} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+    );
+
+    return (
+        <Box sx={{ display: 'flex' }} >
+            <CssBaseline />
+            <AppBar component="nav">
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                        onClick={handleDrawerToggle}
+                        sx={{ mr: 2, display: { sm: 'none' } }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+                    >
+                        Game Builder Workshop
+                    </Typography>
+                    <Box sx={{ display: 'block' }}>
+                        {navItems.map((item) => (
+                            <Button key={item} sx={{ color: '#fff' }}>
+                                {item}
+                            </Button>
+                        ))}
+                    </Box>
+                </Toolbar>
+            </AppBar>
+            <nav>
+                <Drawer
+                    variant="temporary"
+                    open={mobileOpen}
+                    onClose={handleDrawerToggle}
+                    ModalProps={{
+                        keepMounted: true, // Better open performance on mobile.
+                    }}
+                    sx={{
+                        display: { xs: 'block', sm: 'none' },
+                        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                    }}
+                >
+                    {drawer}
+                </Drawer>
+            </nav>
+            <Box component="main" sx={{ p: 3 }}>
+                <Toolbar />
+                {children}
+            </Box>
+        </Box >
+    )
+};
