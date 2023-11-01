5a103861a85789960bee05e19c4437a10169bd44
emmariegeo <57061691+emmariegeo@users.noreply.github.com>
2023-11-01 13:33:00 -0700

    Use provider with store

--- web-app/src/app/page.tsx
+++ web-app/src/app/page.tsx
@@ -5,26 +5,30 @@ import { Container, Grid } from '@mui/material'
 import Toolbox from '@/components/Toolbox'
 import OptionsMenu from '@/components/OptionsMenu'
 import AssetsDrawer from '@/components/AssetsDrawer'
+import { Provider } from 'react-redux'
+import { store } from '@/store'
 
 export default function Home() {
   return (
     <Container >
-      <Navigation>
-        <Container>
-          <Grid container sx={{ height: '100%' }} spacing={0} columns={{ xs: 8, sm: 8, md: 12 }}>
-            <Grid item xs={8} sm={2}>
+      <Provider store={store}>
+        <Navigation>
+          <Container>
+            <Grid container sx={{ height: '100%' }} spacing={0} columns={{ xs: 8, sm: 8, md: 12 }}>
+              <Grid item xs={8} sm={2}>
                 <Toolbox />
+              </Grid>
+              <Grid item xs={12} sm={8} sx={{ overflow: 'hidden', padding: 0 }}>
+                <Canvas />
+              </Grid>
+              <Grid item xs={8} sm={2}>
+                <OptionsMenu />
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={8} sx={{ overflow: 'hidden', padding: 0 }}>
-              <Canvas />
-            </Grid>
-            <Grid item xs={8} sm={2}>
-              <OptionsMenu/>
-            </Grid>
-          </Grid>
-        </Container>
-      </Navigation>
-      <AssetsDrawer/>
+          </Container>
+        </Navigation>
+        <AssetsDrawer />
+      </Provider>
     </Container>
   )
 }
