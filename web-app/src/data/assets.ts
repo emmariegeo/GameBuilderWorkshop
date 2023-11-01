3a2f8ef8260d94688d4b4df7fadb373064a8ff71
emmariegeo <57061691+emmariegeo@users.noreply.github.com>
2023-11-01 13:32:25 -0700

    Reorganize asset data

--- web-app/src/data/assets.ts
+++ web-app/src/data/assets.ts
@@ -1,76 +1,80 @@
-export const data = {
-    "backgrounds": {
-        "bg1": {
+interface Map {
+    [key: string]: { [id: string]: { img: string, title?: string } }
+}
+
+export const data: Map = {
+    backgrounds: {
+        bg1: {
             "img": "assets/backgrounds/bluesky.png",
             "title": "blue sky"
         },
-        "bg2": {
+        bg2: {
             "img": "assets/backgrounds/cavern.png",
             "title": "cavern"
         },
-        "bg3": {
+        bg3: {
             "img": "assets/backgrounds/forest.png",
             "title": "forest"
         },
-        "bg4": {
+        bg4: {
             "img": "assets/backgrounds/magicalcave.png",
             "title": "magical cave"
         },
-        "bg5": {
+        bg5: {
             "img": "assets/backgrounds/shipwreck.png",
             "title": "shipwreck"
         },
-        "bg6": {
+        bg6: {
             "img": "assets/backgrounds/starryforest.png",
             "title": "starry forest"
         },
-        "bg7": {
+        bg7: {
             "img": "assets/backgrounds/starrysky.png",
             "title": "starry sky"
         },
-        "bg8": {
+        bg8: {
             "img": "assets/backgrounds/sunsetocean.png",
             "title": "sunset ocean"
         },
-        "bg9": {
+        bg9: {
             "img": "assets/backgrounds/treasurecave.png",
             "title": "treasure cave"
         },
-        "bg10": {
+        bg10: {
             "img": "assets/backgrounds/undersea.png",
             "title": "undersea"
         },
-        "bg11": {
+        bg11: {
             "img": "assets/backgrounds/water.png",
             "title": "water"
         },
     },
-    "sprites": {
-        "bg1": {
+    sprites: {
+        bg1: {
             "img": "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
             "title": "bg1"
         }
     },
-    "items": {
-        "bg1": {
+    items: {
+        bg1: {
             "img": "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
             "title": "bg1"
         }
     },
-    "obstacles": {
-        "bg1": {
+    obstacles: {
+        bg1: {
             "img": "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
             "title": "bg1"
         }
     },
-    "effects": {
-        "bg1": {
+    effects: {
+        bg1: {
             "img": "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
             "title": "bg1"
         }
     },
-    "audio": {
-        "bg1": {
+    audio: {
+        bg1: {
             "img": "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
             "title": "bg1"
         }
